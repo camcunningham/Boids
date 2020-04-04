4ba150bcdb6a3c61ad8b35341c1a35e8e5057e14
Cam Cunningham <35742414+camcunningham@users.noreply.github.com>
2020-04-03 21:30:43 -0400

    Outlined the boid class

--- boid.py
+++ boid.py
@@ -1,4 +1,14 @@
 class Boid:
-    pass
+    # A boid has a position, velocity, and unique identifier
+    def __init__(self, position, velocity, identifier):
+        self.position = position
+        self.velocity = velocity
+        self.id = identifier
 
-# This will be where the boid class goes
+    # Function that updates the velocity of a boid
+    def addVelocity(self, vel):
+        self.velocity = (self.velocity[0] + vel[0], self.velocity[1] + vel[1])
+
+    # Function that applied movement from velocity to the position of the boid
+    def move(self):
+        self.position = (self.position[0] + self.velocity[0], self.position[1] + self.velocity[1])
