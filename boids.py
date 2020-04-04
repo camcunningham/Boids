0d0c76112843e87cef8ee7d62928df53247c3acb
Cam Cunningham <35742414+camcunningham@users.noreply.github.com>
2020-04-04 14:05:22 -0400

    Added shells for rules, implemented rule 1

--- boids.py
+++ boids.py
@@ -57,12 +57,51 @@ PROCEDURE bound_position(Boid b)
 	RETURN v
 END PROCEDURE
 '''
-# Function that moves the boids based on their velocity
+# Function that moves the boids based on their velocity: Runs once then repeats every 25ms
 def moveBoids():
+    # Update boid position based on rules
+    updateBoidPositions()
     for boid in boids:
+        # Move on screen
         canvas.move(boid.shape, boid.velocity[0], boid.velocity[1])
-
+    # Run this function again
     root.after(25,moveBoids)
+
+def updateBoidPositions():
+    for boid in boids:
+        v1 = rule1(boid)
+        v2 = rule2(boid)
+        v3 = rule3(boid)
+        v4 = edgeRule(boid)
+
+        # Apply new changes to boids velocity
+        boid.velocity = (boid.velocity[0] + v1[0] + v2[0] + v3[0] + v4[0], boid.velocity[1] + v1[1] + v2[1] + v3[1] + v4[1])
+
+# Temp return values for these right now
+
+# Rule 1: Boids try to fly towards the center of mass of neighbouring boids
+def rule1(b):
+    changeInVel = (0,0)
+    for boid in boids:
+        if boid is not b:
+            changeInVel = (changeInVel[0] + boid.position[0], changeInVel[1] + boid.position[1])
+    changeInVel = (changeInVel[0] / len(boids) - 1, changeInVel[1] / len(boids) - 1)
+    # Making the change smaller: Dividing it by 100 makes it move 1% of the way towards center
+    return ( (changeInVel[0] - b.position[0]) / 100 , (changeInVel[0] - b.position[0]) / 100 )
+
+# Rule 2: Boids keep a small distance between them and other boids
+def rule2(b):
+    return (0.25, 0.25)
+
+def rule3(b):
+    return (0.25,0.25)
+
+def edgeRule(b):
+    return (0.25,0.25)
+
+
+
+
 
 root = Tk()
 
